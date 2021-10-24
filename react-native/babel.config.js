0b7a973bac0334ca6787312dd2c311e07314f8f9
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-10-24 17:50:47 -0300

    chore: setup babel module resolver with TS paths

--- react-native/babel.config.js
+++ react-native/babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.ts', '.tsx'],
+          alias: {
+            src: './src',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+          },
+        },
+      ],
+    ],
   };
 };
